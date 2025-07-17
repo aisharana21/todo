9a10f34af78ab2ebbfeb547d614f7de03a53266c
aisharana21 <rana.ayesha590@gmail.com>
2025-07-17 15:40:41 +0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,11 +1,11 @@
-    let todoArray = [];
-    function renderTodo() {
-      let todo = document.querySelector(".todo-list-container");
-      let todoHtml = "";
+let todoArray = [];
+function renderTodo() {
+  let todo = document.querySelector(".todo-list-container");
+  let todoHtml = "";
 
-      todoArray.forEach((todo, index) => {
+  todoArray.forEach((todo, index) => {
 
-        todoHtml += `
+    todoHtml += `
         
         
         <div class="todo-list" >
@@ -33,93 +33,104 @@
             <span class="todo-save js-todo-save" data-index="${index}">Save</span>
            </div>
            `
-        console.log("array current", todoArray);
+    console.log("array current", todoArray);
 
 
-      })
-      todo.innerHTML = todoHtml;
-      //aadding update inside rendor so each time the page load event listener also loads
-      //selecting all todo buttons from which clicked updated button will work
-      document.querySelectorAll(".js-todo-update-button").
-        forEach((btn) => {
-          btn.addEventListener('click', () => {
-            console.log("Im a listenrer");
-            // storing index number from the update button index number
-            const btnIndex = btn.dataset.index;
-            console.log(btnIndex);
-            // created a container variable which will have button class with the selected index
-            const updateContainer = document.querySelector(`.todo-button-${btnIndex}`);
-            console.log(updateContainer);
-            updateContainer.classList.add("show-update");
-          });
+  })
+  todo.innerHTML = todoHtml;
+  //aadding update inside rendor so each time the page load event listener also loads
+  //selecting all todo buttons from which clicked updated button will work
+  document.querySelectorAll(".js-todo-update-button").
+    forEach((btn) => {
+      btn.addEventListener('click', () => {
+        console.log("Im a listener");
+        // storing index number from the update button index number
+        const btnIndex = btn.dataset.index;
+        console.log(btnIndex);
+        // created a container variable which will have button class with the selected index
+        const updateContainer = document.querySelector(`.todo-button-${btnIndex}`);
+        console.log(updateContainer);
+        updateContainer.classList.add("show-update");
+      });
 
-        });
-      // saving todo
-      document.querySelectorAll(".js-todo-save").
-        forEach((btn) => {
-          btn.addEventListener('click', () => {
-            const btnIndex = btn.dataset.index;
-            const updateContainer = document.querySelector(`.todo-button-${btnIndex}`);
-            console.log(updateContainer);
+    });
+  // saving todo
+  document.querySelectorAll(".js-todo-save").
+    forEach((btn) => {
+      btn.addEventListener('click', () => {
+        const btnIndex = btn.dataset.index;
+        const updateContainer = document.querySelector(`.todo-button-${btnIndex}`);
+        console.log(updateContainer);
 
-            updateContainer.classList.remove("show-update");
-            console.log("save done");
-            let updatedTodo = updateContainer.querySelector(".js-todo-update-input").value;
-            let updatedDate = updateContainer.querySelector(".js-todo-update-date").value;
-            console.log(updatedTodo);
-            console.log(updatedDate);
-            if (updatedTodo === "" && updatedDate === "") {
-              todoArray[btnIndex].todoVal = todoArray[todoVal];
-              todoArray[btnIndex].todoDate = todoArray[todoDate];
+        updateContainer.classList.remove("show-update");
+        console.log("save done");
+        let updatedTodo = updateContainer.querySelector(".js-todo-update-input").value;
+        let updatedDate = updateContainer.querySelector(".js-todo-update-date").value;
+        console.log(updatedTodo);
+        console.log(updatedDate);
+        if (updatedTodo === "" && updatedDate === "") {
+         todoArray[btnIndex].todoVal = todoArray[btnIndex].todoVal;
+  todoArray[btnIndex].todoDate = todoArray[btnIndex].todoDate;
+        }
+        //only changing todo
+        else if (updatedTodo !== "" && updatedDate === "") {
+           todoArray[btnIndex].todoVal = updatedTodo;
 
-            }
-            else {
-              todoArray[btnIndex].todoVal = updatedTodo;
-              todoArray[btnIndex].todoDate = updatedDate;
-            }
-            saveTodo();
-            renderTodo();
-          });
 
-        });
-    }
+       }
+       //only changing date
+        else if (updatedDate !== "" && updatedTodo==="") {
+  todoArray[btnIndex].todoDate = updatedDate;
 
-    function addTodo() {
-
-      let todoVal = document.getElementById("todo").value;
-
-      let todoDate = document.getElementById("todoDate").value;
-      if (todoVal == "" && todoDate == "") {
-        document.querySelector(".todo-list-container").innerHTML = "<p>Enter a todo</p>";
-
-      }
-      else {
-
-        todoArray.push({ todoVal, todoDate });
-        document.getElementById("todo").value = "";
-        document.getElementById("todoDate").value = "";
+        }
+// changing both
+        else {
+           todoArray[btnIndex].todoVal = updatedTodo;
+  todoArray[btnIndex].todoDate = updatedDate;
+        }
         saveTodo();
         renderTodo();
-      }
-    }
-    function deleteTodo(index) {
-      console.log("I am deleted todo ", index);
+      });
 
-      todoArray.splice(index, 1);
-      saveTodo();
-      renderTodo();
-    }
+    });
+}
 
-    function saveTodo() {
-      localStorage.setItem('todo', JSON.stringify(todoArray));
+function addTodo() {
 
-    }
-    function loadFromLocalStorage() {
-      const storedTodos = JSON.parse(localStorage.getItem("todo"));
+  let todoVal = document.getElementById("todo").value;
 
-      if (storedTodos) {
-        todoArray = storedTodos;
-        renderTodo();
-      }
-    }
-    loadFromLocalStorage();
+  let todoDate = document.getElementById("todoDate").value;
+  if (todoVal == "" && todoDate == "") {
+    document.querySelector(".todo-list-container").innerHTML = "<p>Enter a todo</p>";
+
+  }
+  else {
+
+    todoArray.push({ todoVal, todoDate });
+    document.getElementById("todo").value = "";
+    document.getElementById("todoDate").value = "";
+    saveTodo();
+    renderTodo();
+  }
+}
+function deleteTodo(index) {
+  console.log("I am deleted todo ", index);
+
+  todoArray.splice(index, 1);
+  saveTodo();
+  renderTodo();
+}
+
+function saveTodo() {
+  localStorage.setItem('todo', JSON.stringify(todoArray));
+
+}
+function loadFromLocalStorage() {
+  const storedTodos = JSON.parse(localStorage.getItem("todo"));
+
+  if (storedTodos) {
+    todoArray = storedTodos;
+    renderTodo();
+  }
+}
+loadFromLocalStorage();
